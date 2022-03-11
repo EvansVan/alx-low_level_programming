b9058f37cd88b47f93ea44deadca7fce494f9ffe
EvansVan <evansndiritu@gmail.com>
2022-03-11 10:31:24 -0800

    fix2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,18 +11,18 @@
 int main(void)
 {
 	int i, j;
-
+	
 	i = 97;
 	j = 65;
 
 	while (i < 123)	
-	{
-		putchar(i);									
+	{	
+		putchar(i);
 		i++;
 	}
 	while (j < 91)
 	{
-		putchar(j);									
+		putchar(j);
 		j++;
 	}
 	putchar(10);
