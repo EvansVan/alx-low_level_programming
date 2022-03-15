42111c5bbec029110f10eb2f1cb2c383c553691a
EvansVan <evansndiritu@gmail.com>
2022-03-15 13:40:05 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *  Return:0
+ */
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
