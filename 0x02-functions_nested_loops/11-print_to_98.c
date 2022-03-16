0f9926e17763ed9f3d639fca37f7b072b15ad9ca
EvansVan <evansndiritu@gmail.com>
2022-03-16 02:46:19 -0700

    hotfix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,7 @@
  * print_to_98 - prints numbers to 98
  * @n: starting integer
  * Description: prints all natural number from n - 98
- * 
+ *
  * Return: void
  */
 
@@ -24,7 +24,7 @@ void print_to_98(int n)
 		while (n > 98)
 		{
 			printf("%d, ", n);
-			n++;
+			n--;
 		}
 	}
 	printf("98\n");
