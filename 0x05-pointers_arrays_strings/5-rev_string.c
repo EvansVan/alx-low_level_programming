4a19a542e2a0a218aea0453e52b4d6d15728ae2e
EvansVan <evansndiritu@gmail.com>
2022-03-21 16:39:45 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int i , count, c2;
+
+	count = 0;
+	c2 = 0;
+
+	while (s[count] != '\0')
+	{
+		count++
+	}
+
+	c2 = count - 1;
+
+	for (i = 0; i < count / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[c2];
+		s[c2--] = temp;
+	}
+}
