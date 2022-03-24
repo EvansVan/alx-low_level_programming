e67b85a29723963da37c51e57090f3a9d8977860
EvansVan <evansndiritu@gmail.com>
2022-03-24 11:02:09 -0700

    fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,11 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; j++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
 	}
+	
 	return (s)
 }
