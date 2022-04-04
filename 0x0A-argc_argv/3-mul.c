16e145317caa8f348bb0639ec4f3fb3ced9b4326
EvansVan <evansndiritu@gmail.com>
2022-04-04 11:08:08 -0700

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -38,16 +38,18 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
 	return (n)
 }
+
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 (Success), 1 (Error)
- */
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 (Success), 1 (Error)
+*/
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
